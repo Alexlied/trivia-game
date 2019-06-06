ecf892aa3cd981a2f531366ecb1eed813422e38c
alexlied <alexlied@gmail.com>
2019-06-06 12:31:28 -0700

    color design

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
 
   render() {
       return (
-          <div>
+          <div className="body">
               <Router>
                   <Switch>
                       <Route exact path="/" component={Home} />
